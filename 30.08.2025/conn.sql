891bb572c654f719c69330494336a44a38141c9f
Bharat P <84564946+Bharat1417@users.noreply.github.com>
2025-08-30 16:20:23 +0530

    Add files via upload

---
+++ 30.08.2025/conn.sql
@@ -0,0 +1,3 @@
+use saturday;
+create table employees(emp_id int,emp_name varchar(50),emp_position varchar(50));
+insert into employees value(101,'Sudhakar','Manager'),(102,'gopi','tester'),(103,'patlu','developer');
